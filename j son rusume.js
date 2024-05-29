6c17305b10cb5ad7ddae24c99d9976e3bffa7b01
n anand <anandraodknb12h@gmail.com>
2024-05-29 20:22:58 +0800

    Add files via upload

---
+++ j son rusume.js
@@ -0,0 +1,65 @@
+let myResume={
+    "my data": {
+      "name": "Anand.N",
+      "email": "anandraoddknb12h@gamil.com",
+      "phone": 8778622914,
+      "degree": "B.Com",
+      "location": {
+        "address": "karumadurai,last street",
+        "postalCode":636138,
+        "city": "Salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+
+      "profiles": [
+        {
+          "github":"https://github.com/anand0127ad/resume.git",
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "salem christian collage of arts and science",
+        "department": "bachelor",
+        "studyType": "fulltime",
+        "batch start year":2021,
+        "batch end year": 2023,
+        "gpa": 8.5,
+      }     
+    ],
+
+    "education": [
+        {
+          "institution": "gnana Deepam Matriculation HigherSecoundary School",
+          "department": "12 th",
+          "studyType": "fulltime",
+          "batch start year":2019,
+          "batch end year": 2020,
+          "gpa": 7.5,
+        }     
+      ],
+      "education": [
+        {
+          "institution": "gnana Deepam Matriculation  School",
+          "department": "10th",
+          "studyType": "fulltime",
+          "batch start year":2018,
+          "batch end year": 2018,
+          "gpa": 7.0,
+        }     
+      ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "read only interting new only,play foot ball,read books,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
